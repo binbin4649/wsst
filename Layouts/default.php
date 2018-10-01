d2ba7b4f67d6e82a229f24ee78571f12171895a3
hideichi saito <binbin4649@gmail.com>
2018-10-01 19:23:50 +0900

    little fix.

--- Layouts/default.php
+++ Layouts/default.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<?php $this->BcBaser->charset() ?>
 	<?php $this->BcBaser->title() ?>
-	<?php $this->Ogp->showOgp() ?>
+	<?php if(!empty($this->Ogp)) $this->Ogp->showOgp(); ?>
 	<?php $this->BcBaser->js(array()); ?>
 	<?php $this->BcBaser->scripts() ?>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -20,6 +20,14 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.loadingoverlay/latest/loadingoverlay.min.js"></script>
+	<script>
+      $(function() {
+          $("form").on('submit', function(){
+              $.LoadingOverlay("show");
+          });
+      });
+    </script>
 	<!-- /Elements/google_analytics.php -->
 	<?php $this->BcBaser->googleAnalytics() ?>
 </head>
